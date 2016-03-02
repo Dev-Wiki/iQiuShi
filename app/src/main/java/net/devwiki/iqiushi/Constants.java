a7b526ac669004704dac8ab8ebe16ae29ddec796
live <bozhou2010@live.com>
2016-03-03 01:04:43 +0800

    初始化项目...

---
+++ app/src/main/java/net/devwiki/iqiushi/Constants.java
@@ -0,0 +1,16 @@
+package net.devwiki.iqiushi;
+
+/**
+ * 常量接口
+ * Created by Asia on 2016/3/3 0003.
+ */
+public interface Constants {
+
+    String URL_HOST = "";
+
+    String URL_ALL_TEXT = "";
+
+    String URL_ALL_PIC = "";
+
+    String URL_TEXT_PIC = "";
+}
