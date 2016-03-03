eb002c3f6c0411df104336b7cf717acacee3c9f1
zyz <974952098@qq.com>
2016-03-03 20:55:42 +0800

    增加自定义View,拆分布局

--- app/src/main/java/net/devwiki/iqiushi/widget/FunctionView.java
+++ app/src/main/java/net/devwiki/iqiushi/widget/FunctionView.java
@@ -2,12 +2,18 @@ package net.devwiki.iqiushi.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
+
+import net.devwiki.iqiushi.R;
 
 /**
  * Created by zyz on 2016/3/3.
  */
 public class FunctionView extends LinearLayout {
+
+    private View rootView;
 
     public FunctionView(Context context) {
         super(context);
@@ -15,9 +21,15 @@ public class FunctionView extends LinearLayout {
 
     public FunctionView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initView(context);
     }
 
     public FunctionView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    private void initView(Context context){
+        rootView = LayoutInflater.from(context).inflate(R.layout.function_view, this, true);
     }
 }
--- app/src/main/java/net/devwiki/iqiushi/widget/NavView.java
+++ app/src/main/java/net/devwiki/iqiushi/widget/NavView.java
@@ -2,12 +2,18 @@ package net.devwiki.iqiushi.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
+
+import net.devwiki.iqiushi.R;
 
 /**
  * Created by zyz on 2016/3/3.
  */
 public class NavView extends RelativeLayout {
+
+    private View rootView;
 
     public NavView(Context context) {
         super(context);
@@ -15,9 +21,15 @@ public class NavView extends RelativeLayout {
 
     public NavView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initView(context);
     }
 
     public NavView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    private void initView(Context context){
+        rootView = LayoutInflater.from(context).inflate(R.layout.nav_view, this, true);
     }
 }
